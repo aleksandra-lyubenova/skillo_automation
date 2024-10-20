6239a145a8443870abf49d93d179e2b2163568eb
aleksandra-lyubenova <a.liubenova@abv.bg>
2024-10-20 17:51:31 +0300

    Eligible to work

--- src/main/java/org/example/hwfour/Exercise_Three.java
+++ src/main/java/org/example/hwfour/Exercise_Three.java
@@ -1,4 +1,17 @@
-package Operators_expressions_stetements;
+package main.java.org.example.hwfour;
+
+import java.util.Scanner;
 
 public class Exercise_Three {
+    public static void main(String[] args) {
+        //3. Write Java program to allow the user to input his/her age. Then the program
+        //will show if the person is eligible to work. A person who is eligible to work
+        //must be older than or equal to 16 years old.
+
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if (age >= 16){
+            System.out.println("User is eligible to work.");
+        } else System.out.println("User is not eligible to work.");
+    }
 }
